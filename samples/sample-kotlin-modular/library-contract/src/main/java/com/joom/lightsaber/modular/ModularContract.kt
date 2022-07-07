8d208ec576ac208beacf392b4203a9d4db66951f
Dmitry Kostyrev <dkostyrev@joom.com>
2022-07-07 15:33:09 +0300

    Introduce sample-kotlin-modular.

---
+++ samples/sample-kotlin-modular/library-contract/src/main/java/com/joom/lightsaber/modular/ModularContract.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2022 SIA Joom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.joom.lightsaber.modular
+
+import com.joom.lightsaber.ContractConfiguration
+import com.joom.lightsaber.Import
+
+interface ModularContract {
+  val componentDependency: ComponentDependency
+  val moduleDependency: ModuleDependency
+}
+
+class ModularContractConfiguration : ContractConfiguration<ModularContract>() {
+  @Import
+  fun importModule(): LibraryModule {
+    return LibraryModule()
+  }
+
+  @Import
+  fun importComponent(): LibraryComponent {
+    return LibraryComponent()
+  }
+}
