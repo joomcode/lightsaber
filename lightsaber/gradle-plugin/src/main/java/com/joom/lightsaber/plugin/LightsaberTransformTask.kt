35bc7d906f729d39c05eb96fe7eb0bdcf07ebab3
Dmitry Kostyrev <dkostyrev@joom.com>
2022-07-08 12:33:13 +0300

    AndroidLightsaberPlugin uses Variant API to register LightsaberTransformTask instead of Transfromation API.

---
+++ lightsaber/gradle-plugin/src/main/java/com/joom/lightsaber/plugin/LightsaberTransformTask.kt
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2022 SIA Joom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.joom.lightsaber.plugin
+
+import com.joom.lightsaber.processor.LightsaberParameters
+import com.joom.lightsaber.processor.LightsaberProcessor
+import org.gradle.api.DefaultTask
+import org.gradle.api.GradleScriptException
+import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.file.Directory
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.logging.LogLevel
+import org.gradle.api.provider.ListProperty
+import org.gradle.api.tasks.CacheableTask
+import org.gradle.api.tasks.Classpath
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.TaskAction
+
+@CacheableTask
+abstract class LightsaberTransformTask : DefaultTask() {
+
+  @get:InputFiles
+  @get:Classpath
+  abstract val inputClasses: ListProperty<Directory>
+
+  @get:InputFiles
+  @get:Classpath
+  abstract val bootClasspath: ConfigurableFileCollection
+
+  @get:InputFiles
+  @get:Classpath
+  abstract val classpath: ConfigurableFileCollection
+
+  @get:InputFiles
+  @get:Classpath
+  abstract val modulesClasspath: ConfigurableFileCollection
+
+  @get:OutputDirectory
+  abstract val outputDirectory: DirectoryProperty
+
+  init {
+    logging.captureStandardOutput(LogLevel.LIFECYCLE)
+  }
+
+  @TaskAction
+  fun process() {
+    val output = outputDirectory.get().asFile.toPath()
+    val parameters = LightsaberParameters(
+      inputs = inputClasses.get().map { it.asFile.toPath() },
+      outputs = List(inputClasses.get().size) { output },
+      classpath = classpath.map { it.toPath() },
+      bootClasspath = bootClasspath.map { it.toPath() },
+      modulesClasspath = modulesClasspath.map { it.toPath() },
+      gen = output,
+      projectName = (project.path + name.replace(TASK_PREFIX, ":")).replace(':', '$'),
+    )
+
+    logger.info("Starting Lightsaber processor: {}", parameters)
+
+    val processor = LightsaberProcessor(parameters)
+    try {
+      processor.process()
+    } catch (exception: Exception) {
+      throw GradleScriptException("Lightsaber processor failed to process files", exception)
+    }
+  }
+
+  companion object {
+    const val TASK_PREFIX = "lightsaberTransformClasses"
+  }
+}
