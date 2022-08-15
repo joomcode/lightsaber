b5e63f55602e0a6c34e4a9c905bea4eb197bccb2
Dmitry Kostyrev <dkostyrev@joom.com>
2022-08-15 17:16:58 +0300

    Introduce LightsaberSharedBuildCacheImpl.

--- lightsaber/processor/src/main/java/com/joom/lightsaber/processor/LightsaberSharedBuildCache.kt
+++ lightsaber/processor/src/main/java/com/joom/lightsaber/processor/LightsaberSharedBuildCache.kt
@@ -16,8 +16,32 @@
 
 package com.joom.lightsaber.processor
 
+import com.joom.lightsaber.processor.commons.closeQuietly
 import java.io.Closeable
+import java.util.concurrent.ConcurrentHashMap
 
 interface LightsaberSharedBuildCache : Closeable {
   fun <K : Any, V : Closeable> getOrPut(key: K, factory: (key: K) -> V): V
+
+  companion object {
+    fun create(): LightsaberSharedBuildCache {
+      return LightsaberSharedBuildCacheImpl()
+    }
+  }
+}
+
+internal class LightsaberSharedBuildCacheImpl : LightsaberSharedBuildCache {
+  private val cache = ConcurrentHashMap<Any, Closeable>()
+
+  override fun <K : Any, V : Closeable> getOrPut(key: K, factory: (key: K) -> V): V {
+    @Suppress("UNCHECKED_CAST")
+    return (cache.getOrPut(key) { factory(key) } as V)
+  }
+
+  override fun close() {
+    cache.forEach {
+      it.value.closeQuietly()
+    }
+    cache.clear()
+  }
 }
