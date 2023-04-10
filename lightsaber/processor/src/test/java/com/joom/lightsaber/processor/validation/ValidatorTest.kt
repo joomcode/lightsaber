8a6f4bd4e203b988318c8a6f40f655c0c264e533
Vladimir Mironov <nsk.mironov@gmail.com>
2023-04-10 13:01:52 +0100

    Tweak error messages in ValidatorTest.

--- lightsaber/processor/src/test/java/com/joom/lightsaber/processor/validation/ValidatorTest.kt
+++ lightsaber/processor/src/test/java/com/joom/lightsaber/processor/validation/ValidatorTest.kt
@@ -53,9 +53,11 @@ class ValidatorTest {
   fun test_validation_fails_if_provided_as_is_used_without_provided_by() {
     integrationTestRule.assertInvalidProject(
       sourceCodeDir = "provided_as_without_provided_by",
-      message = "Invalid configuration for dependency: test_case_projects.validator.provided_as_without_provided_by.DependencyInterface. \n" +
-          "\ttest_case_projects.validator.provided_as_without_provided_by.DependencyImplementation: Using @ProvidedAs without @ProvidedBy." +
-          " You must either add @ProvidedBy annotation or add @Provide method\n"
+      message = """
+        Invalid configuration for dependency: test_case_projects.validator.provided_as_without_provided_by.DependencyInterface. 
+        	test_case_projects.validator.provided_as_without_provided_by.DependencyImplementation: Using @ProvidedAs without @ProvidedBy. You must either add @ProvidedBy annotation or add @Provide method
+
+      """.trimIndent()
     )
   }
 
@@ -63,11 +65,11 @@ class ValidatorTest {
   fun test_validation_fails_if_provided_by_is_used_without_inject() {
     integrationTestRule.assertInvalidProject(
       sourceCodeDir = "provided_by_without_inject",
-      message = "Invalid configuration for dependency: " +
-          "test_case_projects.validator.provided_by_without_inject.DependencyInterface. \n" +
-          "\ttest_case_projects.validator.provided_by_without_inject.DependencyImplementation: " +
-          "Using @ProvidedBy without @Inject. You must either add at least one constructor " +
-          "with @Inject annotation or add @Provide method\n"
+      message = """
+        Invalid configuration for dependency: test_case_projects.validator.provided_by_without_inject.DependencyInterface. 
+        	test_case_projects.validator.provided_by_without_inject.DependencyImplementation: Using @ProvidedBy without @Inject. You must either add at least one constructor with @Inject annotation or add @Provide method
+
+      """.trimIndent()
     )
   }
 
@@ -75,11 +77,12 @@ class ValidatorTest {
   fun test_validation_fails_if_dependency_is_not_provided() {
     integrationTestRule.assertInvalidProject(
       sourceCodeDir = "dependency_missed",
-      message = "Unresolved dependency" +
-          " test_case_projects.validator.dependency_missed.MissedDependencyInterface:\n" +
-          "  ContractConfiguration: test_case_projects.validator.dependency_missed.AppContractConfiguration\n" +
-          "  Contract: test_case_projects.validator.dependency_missed.AppContract\n" +
-          "  Method: test_case_projects.validator.dependency_missed.MissedDependencyInterface getDependency()"
+      message = """
+        Unresolved dependency test_case_projects.validator.dependency_missed.MissedDependencyInterface:
+          ContractConfiguration: test_case_projects.validator.dependency_missed.AppContractConfiguration
+          Contract: test_case_projects.validator.dependency_missed.AppContract
+          Method: test_case_projects.validator.dependency_missed.MissedDependencyInterface getDependency()
+      """.trimIndent()
     )
   }
 
@@ -87,10 +90,12 @@ class ValidatorTest {
   fun test_validation_fails_if_dependencies_has_cycles() {
     integrationTestRule.assertInvalidProject(
       sourceCodeDir = "dependency_cycle",
-      message = "Dependency cycle in contract test_case_projects.validator.dependency_cycle.AppContractConfiguration:\n" +
-          "  test_case_projects.validator.dependency_cycle.Dependency1\n" +
-          "  test_case_projects.validator.dependency_cycle.Dependency2\n" +
-          "  test_case_projects.validator.dependency_cycle.Dependency1"
+      message = """
+        Dependency cycle in contract test_case_projects.validator.dependency_cycle.AppContractConfiguration:
+          test_case_projects.validator.dependency_cycle.Dependency1
+          test_case_projects.validator.dependency_cycle.Dependency2
+          test_case_projects.validator.dependency_cycle.Dependency1
+      """.trimIndent()
     )
   }
 
@@ -98,45 +103,51 @@ class ValidatorTest {
   fun test_validation_fails_if_dependency_provided_multiple_times() {
     integrationTestRule.assertInvalidProject(
       sourceCodeDir = "dependency_duplicates",
-      message = "Dependency test_case_projects.validator.dependency_duplicates.Dependency provided multiple times in a contract:\n" +
-          "1.\n" +
-          "  ContractConfiguration: test_case_projects.validator.dependency_duplicates.AppContractConfiguration\n" +
-          "  Method: test_case_projects.validator.dependency_duplicates.AppModule1 importAppModule1()\n" +
-          "  Module: test_case_projects.validator.dependency_duplicates.AppModule1\n" +
-          "  Method: test_case_projects.validator.dependency_duplicates.Dependency provideDependency()\n" +
-          "2.\n" +
-          "  ContractConfiguration: test_case_projects.validator.dependency_duplicates.AppContractConfiguration\n" +
-          "  Method: test_case_projects.validator.dependency_duplicates.AppModule2 importAppModule2()\n" +
-          "  Module: test_case_projects.validator.dependency_duplicates.AppModule2\n" +
-          "  Method: test_case_projects.validator.dependency_duplicates.Dependency provideDependency()"
+      message = """
+        Dependency test_case_projects.validator.dependency_duplicates.Dependency provided multiple times in a contract:
+        1.
+          ContractConfiguration: test_case_projects.validator.dependency_duplicates.AppContractConfiguration
+          Method: test_case_projects.validator.dependency_duplicates.AppModule1 importAppModule1()
+          Module: test_case_projects.validator.dependency_duplicates.AppModule1
+          Method: test_case_projects.validator.dependency_duplicates.Dependency provideDependency()
+        2.
+          ContractConfiguration: test_case_projects.validator.dependency_duplicates.AppContractConfiguration
+          Method: test_case_projects.validator.dependency_duplicates.AppModule2 importAppModule2()
+          Module: test_case_projects.validator.dependency_duplicates.AppModule2
+          Method: test_case_projects.validator.dependency_duplicates.Dependency provideDependency()
+      """.trimIndent()
     )
   }
 
   @Test
   fun validation_fails_if_module_imported_multiple_times() {
     integrationTestRule.assertInvalidProject(
-      "module_duplicates",
-      "Class test_case_projects.validator.module_duplicates.AppModule imported multiple times in a contract:\n" +
-          "1.\n" +
-          "  ContractConfiguration: test_case_projects.validator.module_duplicates.AppContractConfiguration\n" +
-          "  Method: test_case_projects.validator.module_duplicates.AppModule importAppModule1()\n" +
-          "2.\n" +
-          "  ContractConfiguration: test_case_projects.validator.module_duplicates.AppContractConfiguration\n" +
-          "  Method: test_case_projects.validator.module_duplicates.AppModule importAppModule2()"
+      sourceCodeDir = "module_duplicates",
+      message = """
+        Class test_case_projects.validator.module_duplicates.AppModule imported multiple times in a contract:
+        1.
+          ContractConfiguration: test_case_projects.validator.module_duplicates.AppContractConfiguration
+          Method: test_case_projects.validator.module_duplicates.AppModule importAppModule1()
+        2.
+          ContractConfiguration: test_case_projects.validator.module_duplicates.AppContractConfiguration
+          Method: test_case_projects.validator.module_duplicates.AppModule importAppModule2()
+      """.trimIndent()
     )
   }
 
   @Test
   fun validation_fails_if_contract_imported_multiple_times() {
     integrationTestRule.assertInvalidProject(
-      "contract_duplicates",
-      "Class test_case_projects.validator.contract_duplicates.LazyContract imported multiple times in a contract:\n" +
-          "1.\n" +
-          "  ContractConfiguration: test_case_projects.validator.contract_duplicates.AppContractConfiguration\n" +
-          "  Field: test_case_projects.validator.contract_duplicates.LazyContract lazyContract1\n" +
-          "2.\n" +
-          "  ContractConfiguration: test_case_projects.validator.contract_duplicates.AppContractConfiguration\n" +
-          "  Field: com.joom.lightsaber.Lazy<test_case_projects.validator.contract_duplicates.LazyContract> lazyContract2"
+      sourceCodeDir = "contract_duplicates",
+      message = """
+        Class test_case_projects.validator.contract_duplicates.LazyContract imported multiple times in a contract:
+        1.
+          ContractConfiguration: test_case_projects.validator.contract_duplicates.AppContractConfiguration
+          Field: test_case_projects.validator.contract_duplicates.LazyContract lazyContract1
+        2.
+          ContractConfiguration: test_case_projects.validator.contract_duplicates.AppContractConfiguration
+          Field: com.joom.lightsaber.Lazy<test_case_projects.validator.contract_duplicates.LazyContract> lazyContract2
+      """.trimIndent()
     )
   }
 }
